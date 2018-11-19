0251cfa5a65586a7d811801125cde283485f23b6
patriciaf212 <43784801+patriciaf212@users.noreply.github.com>
2018-11-18 19:03:24 -0500

    Update problem-set-4.js

--- js/problem-set-4.js
+++ js/problem-set-4.js
@@ -75,7 +75,7 @@ function fahrenheit() {
   // WRITE YOUR EXERCISE 4 CODE HERE
 var fc = ((fahr - 32) / 1.8).toFixed(2);
 var div = document.getElementById ("output4");
- div.innerHTML = fahr + " " + "degrees Fahrenheit equals" + " " + fc +" degrees Celsius."
+ div.innerHTML = fahr + " " + "degrees Fahrenheit equals" + " " + fc +" degrees Celsius.";
   ///////////////////////////// DO NOT MODIFY
   check("fahrenheit", fahr); // DO NOT MODIFY
   ///////////////////////////// DO NOT MODIFY
@@ -105,16 +105,16 @@ let feet = 0;
 
 miles = inches / 63360;
 miles = Math.floor(miles);
-inches = inches % 63360
-yards = inches / 36
-yards = Math.floor(yards)
-inches = inches % 36
-feet = inches / 12
-feet = Math.floor(feet)
-inches = inches % 12
+inches = inches % 63360;
+yards = inches / 36;
+yards = Math.floor(yards);
+inches = inches % 36;
+feet = inches / 12;
+feet = Math.floor(feet);
+inches = inches % 12;
 
 var div = document.getElementById ("output5");
- div.innerHTML = "Miles: " + miles + "<br/>" + "Yards: " + yards + "<br/>" + "Feet: " + feet + "<br/>" + "Inches: " + inches
+ div.innerHTML = "Miles: " + miles + "<br/>" + "Yards: " + yards + "<br/>" + "Feet: " + feet + "<br/>" + "Inches: " + inches;
   ////////////////////////// DO NOT MODIFY
   check("inches", input); // DO NOT MODIFY
   ////////////////////////// DO NOT MODIFY
@@ -140,11 +140,17 @@ function centimeters() {
   // WRITE YOUR EXERCISE 6 CODE HERE
  let kilometers = 0;
  let meters = 0;
- let centimeters = 0;
 
  kilometers = centimeters / 100000
  kilometers = Math.floor(kilometers)
  centimeters = centimeters % 100000
+ meters = centimeters / 100
+ meters = Math.floor(meters)
+ centimeters = centimeters % 100
+ 
+ var div = document.getElementById ("output6");
+  div.innerHTML = "Kilometers: " + kilometers + "<br/>" + "Meters: " + meters + "<br/>" + "Centimeters: " + centimeters;
+ 
   /////////////////////////////// DO NOT MODIFY
   check("centimeters", input); // DO NOT MODIFY
   /////////////////////////////// DO NOT MODIFY
@@ -168,7 +174,28 @@ function fluidOunces() {
   /////////////////////////// DO NOT MODIFY
 
   // WRITE YOUR EXERCISE 7 CODE HERE
-
+  let gallons = 0;
+  let quarts = 0;
+  let pints = 0;
+  let cups = 0;
+  
+  gallons = fluidOunces / 128;
+  gallons = Math.floor(gallons);
+  fluidOunces = fluidOunces % 128;
+  quarts = fluidOunces / 32;
+  quarts = Math.floor(quarts);
+  fluidOunces = fluidOunces % 32;
+  pints = fluidOunces / 16;
+  pints = Math.floor(pints);
+  fluidOunces = fluidOunces % 16;
+  cups = fluidOunces / 8;
+  cups = Math.floor(cups);
+  fluidOunces = fluidOunces % 8;
+  
+  var div = document.getElementById ("output7");
+   div.innerHTML = "Gallons: " + gallons + "<br/>" + "Quarts: " + quarts + "<br/>" + "Pints: " + pints + "<br/>" + "Cups: " + cups + "<br/>" + "Fluid Ounces: " + fluidOunces;
+  
+  
   /////////////////////////////// DO NOT MODIFY
   check("fluidOunces", input); // DO NOT MODIFY
   /////////////////////////////// DO NOT MODIFY
@@ -192,7 +219,18 @@ function ounces() {
   ////////////////////// DO NOT MODIFY
 
   // WRITE YOUR EXERCISE 8 CODE HERE
-
+  let tons = 0; 
+  let pounds = 0;
+  
+  tons = ounces / 32000;
+  tons = Math.floor(tons);
+  ounces = ounces % 32000;
+  pounds = ounces / 16; 
+  pounds = Math.floor(pounds);
+  ounces = ounces % 16;
+  
+  var div= document.getElementById ("output8");
+   div.innerHTML = "Tons: " + tons + "<br/>" + "Pounds: " + pounds + "<br/>" + "Ounces: " + ounces;
   ////////////////////////// DO NOT MODIFY
   check("ounces", input); // DO NOT MODIFY
   ////////////////////////// DO NOT MODIFY
